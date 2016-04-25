5d1361a5687c2ff1a8ef8d0cb06bb2003877f1aa
hadrianpaulo <hadrianpaulo@gmail.com>
2016-04-26 07:38:37 +0800

    initial RR implementation; Logic isn't complete

--- os.py
+++ os.py
@@ -1,5 +1,5 @@
 """
-Classical algorithms: SJF, FCFS, RR
+Classical algorithms: SJF-NP, FCFS, RR
 Criteria: Response time, waiting time, turnaround time,
 throughput, cpu utilization
 """
@@ -18,6 +18,7 @@ def create_processes():
 
 
 def fcfs(processes):
+    """First Come, First Served"""
     waiting_arr = []
     response_arr = []
     turnaround_arr = []
@@ -27,8 +28,8 @@ def fcfs(processes):
     for i in range(len(processes.index)):
         response_arr.append(response)
         waiting_arr.append(response - processes['new_to_ready'][i])
-        turnaround_arr.append(processes['cpu_bursts'][i]
-                              + processes['new_to_ready'][i] + waiting_arr[i])
+        turnaround_arr.append(processes['cpu_bursts'][i] +
+                              processes['new_to_ready'][i] + waiting_arr[i])
         response += processes['cpu_bursts'][i]
 
     processes['response'] = response_arr
@@ -39,12 +40,38 @@ def fcfs(processes):
 
 
 def sjf_np(processes):
+    """Shortest Job First - Non-preemptive"""
     processes.sort_values(by='cpu_bursts', inplace=True)
     return fcfs(processes).sort_index()
 
 
 def rr(processes, qt):
-    pass
+    """Round Robin"""
+    processes_mut = processes.copy()
+    processes_mut['done'] = False
+
+    waiting_arr = [0] * processes.shape[0]
+    response_arr = [0] * processes.shape[0]
+    turnaround_arr = [0] * processes.shape[0]
+    acc = 0
+    not_completed = True
+
+    response = processes_mut['new_to_ready'][0]
+    while not_completed:
+        for i in range(len(processes.index)):
+            idx = processes_mut.iloc[i].name
+            if processes_mut.iloc[i].cpu_bursts > 0:
+                processes_mut.set_value(idx, 'cpu_bursts', 
+                    processes_mut.iloc[i].cpu_bursts - qt)
+            else:
+                processes_mut.set_value(idx, 'done', True)
+        not_completed = processes_mut[processes_mut.done == True].empty
+
+    processes_mut['response'] = response_arr
+    processes_mut['waiting'] = waiting_arr
+    processes_mut['turnaround'] = turnaround_arr
+
+    return processes_mut.drop('done', axis=1)
 
 
 def main():
