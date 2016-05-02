d91b69cf07b711d9b85db8d669a1234f0bcb0a14
jerelynco <jerelynco@gmail.com>
2016-05-03 00:02:46 +0800

    Added results and modified os.py

--- os.py
+++ os.py
@@ -6,6 +6,13 @@ throughput, cpu utilization
 
 import numpy as np
 import pandas as pd
+import sys
+
+cc = 30
+
+
+def get_throughput(df):
+    return len(df[df['turnaround'] < cc])
 
 
 def create_processes():
@@ -17,7 +24,7 @@ def create_processes():
     return processes
 
 
-def fcfs(processes):
+def fcfs(processes, algo):
     """First Come, First Served"""
     processes = processes.copy()
     waiting_arr = []
@@ -37,17 +44,20 @@ def fcfs(processes):
     processes['waiting'] = waiting_arr
     processes['turnaround'] = turnaround_arr
 
+    print("{}: Throughput of {} with clock cycle benchmark of {}".format(
+        algo, get_throughput(processes), cc))
     return processes
 
 
-def sjf_np(processes):
+def sjf_np(processes, algo):
     """Shortest Job First - Non-preemptive"""
     processes = processes.copy()
     processes.sort_values(by='cpu_bursts', inplace=True)
-    return fcfs(processes).sort_index()
+
+    return fcfs(processes, algo).sort_index()
 
 
-def rr(processes, qt, mean_variant=False, modified_mean_variant=False):
+def rr(processes, qt, algo, mean_variant=False, modified_mean_variant=False):
     """Round Robin"""
     processes_mut = processes.copy()
     processes_mut['started'] = False
@@ -66,9 +76,10 @@ def rr(processes, qt, mean_variant=False, modified_mean_variant=False):
             # Mean round robin
             if mean_variant:
                 qt = np.around(
-                    processes_mut[processes_mut.done == False]['cpu_bursts']
+                    processes_mut[processes_mut.done == False][
+                        'cpu_bursts']
                     .mean())
-            # Modified mean round robin        
+            # Modified mean round robin
             if modified_mean_variant:
                 qt = np.ceil(
                     processes_mut[processes_mut.done == False]['cpu_bursts']
@@ -95,24 +106,56 @@ def rr(processes, qt, mean_variant=False, modified_mean_variant=False):
     processes_mut['cpu_bursts'] = processes['cpu_bursts']
     processes_mut['response'] = response_arr
     processes_mut['turnaround'] = turnaround_arr
-    processes_mut['waiting'] = processes_mut['turnaround'] - processes_mut['cpu_bursts'] - processes_mut['new_to_ready']
+    processes_mut['waiting'] = processes_mut['turnaround'] - \
+        processes_mut['cpu_bursts'] - processes_mut['new_to_ready']
 
+    print("{}: Throughput of {} with clock cycle benchmark of {}".format(
+        algo, get_throughput(processes_mut), cc))
     return processes_mut.drop(['done', 'started'], axis=1)
 
+
 def main():
-    processes = create_processes()
-    fcfs_result = fcfs(processes)
-    sjf_np_result = sjf_np(processes)
-    rr_result = rr(processes, 4)
-    mean_rr_result = rr(processes, 0, mean_variant=True)
-    print("First Come, First Served")
-    print(fcfs_result)
-    print("Shortest Job First - Non-Preemptive")
-    print(sjf_np_result)
-    print("Round Robin - QT = 4")
-    print(rr_result)
-    print("Round Robin with Dynamic QT via Average CPU Burst")
-    print(mean_rr_result)
+    for i in range(3):
+        processes = create_processes()
+
+        print("\n*****Throughput values per algorithm*****")
+
+        ## Redirecting print stream to output stream
+        orig_stdout = sys.stdout
+        f = open("results/throughputs_{}.txt".format(i), 'w')
+        sys.stdout = f
+
+        ## Initializing results // prints the throughputs
+        fcfs_result = fcfs(processes, "fcfs")
+        sjf_np_result = sjf_np(processes, "sfj")
+        rr_result = rr(processes, 4, "rr")
+        orig_mean_rr_result = rr(processes, 0, "orig_mean_rr", mean_variant=True )
+        mod_mean_rr_result = rr(processes, 0, "mod_mean rr", modified_mean_variant=True)
+
+        ## Closing of output stream and returning the print stream
+        sys.stdout = orig_stdout
+        f.close()
+
+        ## Displaying of evaluations
+        print("\n*****Performance Evaluation per Algorithm*****")
+        print("First Come, First Served")
+        print(fcfs_result)
+        print("\nShortest Job First - Non-Preemptive")
+        print(sjf_np_result)
+        print("\nRound Robin - QT = 4")
+        print(rr_result)
+        print("\nRound Robin with Dynamic QT via Average CPU Burst (Original)")
+        print(orig_mean_rr_result)
+        print("\nRound Robin with Dynamic QT via Average CPU Burst (Modified)")
+        print(mod_mean_rr_result)
+
+        ## Saving of results to CSV
+        fcfs_result.to_csv("results/fcfs_result_{}".format(i))
+        sjf_np_result.to_csv("results/sjf_np_result_{}".format(i))
+        rr_result.to_csv("results/rr_result_{}".format(i))
+        orig_mean_rr_result.to_csv("results/orig_mean_rr_result_{}".format(i))
+        mod_mean_rr_result.to_csv("results/mod_mean_rr_result_{}".format(i))
+
 
 if __name__ == '__main__':
     main()
